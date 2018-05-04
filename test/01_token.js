363e1df13b088f8ce65a3acb40fafbcfe13cc50b
tester <user@think.er>
2018-05-04 19:42:39 +0300

    Add tests for constructor

--- test/01_token.js
+++ test/01_token.js
@@ -1,13 +1,35 @@
 var Token = artifacts.require("Token");
 
-contract('Token Basics', function(accounts) {
-	let acc = { owner1: accounts[0], owner2: accounts[1]};
+contract('Token constructor', function(accounts) {
+	let acc = { owner1: accounts[0], owner2: accounts[1], nobody: accounts[9]};
+	let hardcodedTotalSupply = 10000000000000000000000000;
+	let token;
 
-	it('showld have name MMS', async() => {
-
-		let token = await Token.new(acc.owner1, acc.owner2, {from: acc.owner1});
+	it('should have name and symbol set to string("MMS")', async() => {
+		token = await Token.new(acc.owner1, acc.owner2, {from: acc.owner1});
 		let name = await token.name();
+		let symbol = await token.name();
 		assert.equal(name, 'MMS');
+		assert.equal(symbol, 'MMS');
+	});
 
+	it('should have owner and owner2 set to what passed via constructor', async() => {
+		let o1 = await token.owner();
+		let o2 = await token.owner2();
+		assert.equal(o1, acc.owner1);
+		assert.equal(o2, acc.owner2);
+		assert.equal(await token.owner3(), 0); // owner3 set to zero by default
+	});
+
+	it('should have total supply = hardcodedTotalSupply', async() => {
+		assert.equal(await token.totalSupply(), hardcodedTotalSupply);
+	});
+	it('should have initial owner balance equal to hardcodedTotalSupply', async() => {
+		assert.equal(await token.balances(acc.owner1), hardcodedTotalSupply);
+		assert.equal(await token.balanceOf(acc.owner1), hardcodedTotalSupply);
+	});
+	it('should have balanceOf return zero for non-existing address and owner2', async() => {
+		assert.equal(await token.balanceOf(acc.owner2), 0);
+		assert.equal(await token.balanceOf(acc.nobody), 0);
 	});
 });
